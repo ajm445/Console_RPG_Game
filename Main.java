363d50673c0951c9cf78f16945b160adce362bc3
ajm445 <128965664+ajm445@users.noreply.github.com>
2025-05-20 23:11:34 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("""
+                   Welcome to Console RPG Game
+                ***********************************
+                            1. 회원가입
+                             2. 로그인
+                ***********************************
+                Enter number
+                """);
+        System.out.print(">> ");
+        // User.signUp();
+    }
+}
