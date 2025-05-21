7bded25e2b20e2772f9a09005d90d3ef726ce6ac
ajm445 <128965664+ajm445@users.noreply.github.com>
2025-05-21 09:35:04 +0900

    Create UserManager.java

---
+++ UserManager.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class UserManager {
+    public ArrayList<User> users = new ArrayList<>();
+
+    public void addUser(User user) {  // 유저 생성
+        users.add(user);
+    }
+
+    public ArrayList<User> Users() {  // 유저 정보 반환하기
+        return users;
+    }
+
+    public void removeUser(User user) {  // 유저 삭제
+        users.remove(user);
+    }
+
+    public void showAllUsers() {
+        for (User user : users) {
+            user.printInfo();
+        }
+    }
+}
