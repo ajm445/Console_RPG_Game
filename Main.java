cf86445c1b40afd3f37d5102fc22f34f8856ce4d
ajm445 <128965664+ajm445@users.noreply.github.com>
2025-05-21 09:33:54 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,74 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("""
-                   Welcome to Console RPG Game
+    static Scanner in = new Scanner(System.in);
+    public static void mainMenu() {
+        while(true) {
+            if(!User.login) {
+                System.out.print("""
                 ***********************************
+                          Console RPG Game
+                ***********************************
+                            0. 게임종료
                             1. 회원가입
                              2. 로그인
+                           3. 아이디 변경
                 ***********************************
                 Enter number
                 """);
-        System.out.print(">> ");
-        // User.signUp();
+            }
+            else {
+                System.out.print("""
+                ***********************************
+                          Console RPG Game
+                ***********************************
+                            0. 게임종료
+                            1. 게임시작
+                           2. 마이페이지
+                            3. 로그아웃
+                ***********************************
+                Enter number
+                """);
+            }
+            System.out.print(">> ");
+            int check = in.nextInt();
+            in.nextLine();
+            switch (check) {
+                case 0 -> {
+                    System.out.println("게임을 종료합니다.");
+                    System.exit(0);
+                }
+                case 1 -> {
+                    if(!User.login) {
+                        User.signUp();
+                    }
+                    else {
+                        System.out.println("게임시작!");
+                    }
+                }
+                case 2 -> {
+                    if(!User.login) {
+                        User.signIn();
+                    }
+                    else {
+                        System.out.println("마이페이지!");
+                    }
+                }
+                case 3 -> {
+                    if(!User.login) {
+                        User.modifyId();
+                    }
+                    User.login = false;
+                    System.out.println("로그아웃 되었습니다.");
+                    Main.mainMenu();
+                }
+                case 4 -> User.manager.showAllUsers();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("    Welcome to Console RPG Game");
+        mainMenu();
     }
 }
